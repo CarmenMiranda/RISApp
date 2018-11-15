2242f50f0a22cddba6e0b22970cd0bdbb4c850bb
CarmenMiranda <cmiranda@ucol.mx>
2018-11-15 11:20:29 -0600

    Add a component called GroupButtonsContainer

---
+++ src/components/GroupButtonsContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const GroupButtonContainer = (props) => {
+	return (
+		<div style={styles.containerStyle}>
+			{props.children}
+		</div>
+	);
+};
+
+const styles = {
+	containerStyle: {
+		backgroundColor: 'transparent',
+		borderRadius: 8,
+		width: 450,
+		marginLeft: 7
+	}
+}
+export default GroupButtonContainer;
